236f753849a5ddb4a4e11ba290b4c2f2c3735a0a
Irineo-1 <76635517+Irineo-1@users.noreply.github.com>
2024-06-13 21:34:37 -0600

    primer commit. Estructura inicial

---
+++ index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Pagina principal</title>
+</head>
+<body>
+    
+    <!-- Ejemplo de como insertar un codigo aparte -->
+    <?php include 'secciones/ejemplo.php'; ?> 
+
+    <!-- Inicio bienvenida -->
+    <?php include ''; ?> 
+
+    <!-- Quiénes somos -->
+    <?php include ''; ?> 
+
+    <!-- Catálogo de productos y servicios -->
+    <?php include ''; ?> 
+
+    <!-- Clientes -->
+    <?php include ''; ?> 
+
+    <!-- Contáctanos -->
+    <?php include ''; ?> 
+
+</body>
+</html>
---
+++ secciones/ejemplo.php
@@ -0,0 +1,5 @@
+<section>
+
+    <h1>Seccion insertada con php</h1>
+
+</section>
