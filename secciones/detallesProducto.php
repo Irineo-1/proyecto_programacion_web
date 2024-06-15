4a40213271dd000bfb0880c81bf63850d7c68594
Irineo-1 <76635517+Irineo-1@users.noreply.github.com>
2024-06-15 01:33:36 -0600

    example

---
+++ secciones/detallesProducto.php
@@ -0,0 +1,59 @@
+<style>
+    body {
+        font-family: Arial, sans-serif;
+        margin: 0;
+        padding: 0;
+        background-color: #f5f5f5;
+    }
+    .container {
+        width: 80%;
+        max-width: 1200px;
+        margin: 20px auto;
+        background-color: #fff;
+        padding: 20px;
+        box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+        border-radius: 8px;
+    }
+    .product-image {
+        text-align: center;
+    }
+    .product-image img {
+        max-width: 100%;
+        height: auto;
+    }
+    .product-details {
+        margin-top: 20px;
+    }
+    .product-title {
+        font-size: 24px;
+        font-weight: bold;
+    }
+    .product-price {
+        color: #b12704;
+        font-size: 20px;
+        margin-top: 10px;
+    }
+    .product-stock {
+        font-size: 16px;
+        margin-top: 10px;
+        color: #008000;
+    }
+    .product-description {
+        margin-top: 20px;
+        line-height: 1.6;
+    }
+</style>
+
+<div class="container">
+    <div class="product-image">
+        <img src="https://imgs.search.brave.com/eLsVxtv81rOFQe7sOKDmX9oW2JZh-6qbYTLnANedf4Y/rs:fit:500:0:0/g:ce/aHR0cHM6Ly9tZWRp/YS5pc3RvY2twaG90/by5jb20vaWQvMTE1/Mjg1MDExNi9waG90/by9sYXB0b3Atb24t/dGFibGUuanBnP3M9/NjEyeDYxMiZ3PTAm/az0yMCZjPW5wVjVs/ZHJzT0dsZ21XaHRf/U2FIMl9KVzRkU2J6/a0ZzMmJnT1ZZSksy/WFk9" alt="Nombre del Producto">
+    </div>
+    <div class="product-details">
+        <div class="product-title">Laptop</div>
+        <div class="product-price">$3,500</div>
+        <div class="product-stock">Cantidad en existencia: 20</div>
+        <div class="product-description">
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Incidunt perferendis eum voluptates, explicabo cumque vero quibusdam at ipsa vitae maiores harum illo sequi exercitationem excepturi officia, omnis deserunt mollitia non! </p>
+        </div>
+    </div>
+</div>
