457b43b7ca8b70f9d47ae9b56a8924fd0b38d57b
Irineo-1 <villacastillojavieririneo@gmail.com>
2024-06-16 15:01:05 -0600

    avances de la vista

--- secciones/catalogoProductos.php
+++ secciones/catalogoProductos.php
@@ -1,82 +1,97 @@
-<section>
-
-    <style>
-        img {
-            height: 250px !important;
-        }
-    </style>
+<section id="catalogo_productos">
 
     <div class="container">
-        <div class="row">
-            <div class="col">
-                <div class="card" style="width: 18rem;">
-                    <img src="https://imgs.search.brave.com/eLsVxtv81rOFQe7sOKDmX9oW2JZh-6qbYTLnANedf4Y/rs:fit:500:0:0/g:ce/aHR0cHM6Ly9tZWRp/YS5pc3RvY2twaG90/by5jb20vaWQvMTE1/Mjg1MDExNi9waG90/by9sYXB0b3Atb24t/dGFibGUuanBnP3M9/NjEyeDYxMiZ3PTAm/az0yMCZjPW5wVjVs/ZHJzT0dsZ21XaHRf/U2FIMl9KVzRkU2J6/a0ZzMmJnT1ZZSksy/WFk9" class="card-img-top" alt="...">
-                    <div class="card-body">
-                        <h5 class="card-title">Laptop</h5>
-                        <p style="color: green;">$3,500</p>
-                        <p class="card-text">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Libero consectetur commodi adipisci voluptas, hic cum neque, molestias iste ratione repellat ad inventore maxime. Quaerat officiis nemo distinctio vitae, accusantium ut?</p>
-                        <a href="detallesProducto.php" class="btn btn-primary">Ver</a>
-                    </div>
+        <div class="every-row">
+        
+            <div class="card" style="width: 18rem; border: none; margin: 0px 5px;">
+                <img src="https://imgs.search.brave.com/eLsVxtv81rOFQe7sOKDmX9oW2JZh-6qbYTLnANedf4Y/rs:fit:500:0:0/g:ce/aHR0cHM6Ly9tZWRp/YS5pc3RvY2twaG90/by5jb20vaWQvMTE1/Mjg1MDExNi9waG90/by9sYXB0b3Atb24t/dGFibGUuanBnP3M9/NjEyeDYxMiZ3PTAm/az0yMCZjPW5wVjVs/ZHJzT0dsZ21XaHRf/U2FIMl9KVzRkU2J6/a0ZzMmJnT1ZZSksy/WFk9" class="card-img-top" alt="...">
+                <div class="card-body" style="height: 150px;">
+                    <p class="card-text title-productos">
+                        Some quick example text to build on the card title and make up the bulk of the card's content.
+                    </p>
+                    <p class="card-text price">
+                        $9.99
+                    </p>
                 </div>
             </div>
-            <div class="col">
-                <div class="card" style="width: 18rem;">
-                    <img src="https://imgs.search.brave.com/A7vYl0pmZ9_BVoxdcTO1dP0DkbGLIajgkahkvGd6r_w/rs:fit:500:0:0/g:ce/aHR0cHM6Ly93d3cu/c3RmLnRlY2gvY2Ru/L3Nob3AvZmlsZXMv/U1QtSDc4ODE3X0t1/bl9Sb3NlLTFfNTQw/eC5qcGc_dj0xNzEz/MzY2MzQ4" class="card-img-top" alt="...">
-                    <div class="card-body">
-                        <h5 class="card-title">Audifonos</h5>
-                        <p style="color: green;">$2,500</p>
-                        <p class="card-text">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Velit accusantium, facere dignissimos odio reiciendis aliquam quibusdam dolores ad unde. Laudantium, voluptas. Facere saepe natus sint perspiciatis iusto, aperiam quasi tempore?</p>
-                        <a href="#" class="btn btn-primary">Ver</a>
-                    </div>
+            <div class="card" style="width: 18rem; border: none; margin: 0px 5px;">
+                <img src="https://imgs.search.brave.com/eLsVxtv81rOFQe7sOKDmX9oW2JZh-6qbYTLnANedf4Y/rs:fit:500:0:0/g:ce/aHR0cHM6Ly9tZWRp/YS5pc3RvY2twaG90/by5jb20vaWQvMTE1/Mjg1MDExNi9waG90/by9sYXB0b3Atb24t/dGFibGUuanBnP3M9/NjEyeDYxMiZ3PTAm/az0yMCZjPW5wVjVs/ZHJzT0dsZ21XaHRf/U2FIMl9KVzRkU2J6/a0ZzMmJnT1ZZSksy/WFk9" class="card-img-top" alt="...">
+                <div class="card-body" style="height: 150px;">
+                    <p class="card-text title-productos">
+                        Some quick example text to build on the card title and make up the bulk of the card's content.
+                    </p>
+                    <p class="card-text price">
+                        $9.99
+                    </p>
                 </div>
             </div>
-            <div class="col">
-                <div class="card" style="width: 18rem;">
-                    <img src="https://imgs.search.brave.com/Dt2Jfw77ANXPccU-nw1Qnc2Ja_1mizfxdZbqEOPJxSs/rs:fit:500:0:0/g:ce/aHR0cHM6Ly9tZWRp/YS5nZXR0eWltYWdl/cy5jb20vaWQvMTgy/NDk1ODE0L3Bob3Rv/L21vdXNlLmpwZz9z/PTYxMng2MTImdz0w/Jms9MjAmYz1RVlRw/V2h5TGtUUnptb202/cU41aldyLU1jSmVQ/RGU4VDIyOGw1Mkhk/eTJ3PQ" class="card-img-top" alt="...">
-                    <div class="card-body">
-                        <h5 class="card-title">Mouse</h5>
-                        <p style="color: green;">$500</p>
-                        <p class="card-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit dolorum aliquid fugiat maxime, eos dignissimos repellendus unde laboriosam natus tempora in suscipit expedita, aut voluptates quod officiis voluptatibus excepturi. Sit?</p>
-                        <a href="#" class="btn btn-primary">Ver</a>
-                    </div>
+            <div class="card" style="width: 18rem; border: none; margin: 0px 5px;">
+                <img src="https://imgs.search.brave.com/eLsVxtv81rOFQe7sOKDmX9oW2JZh-6qbYTLnANedf4Y/rs:fit:500:0:0/g:ce/aHR0cHM6Ly9tZWRp/YS5pc3RvY2twaG90/by5jb20vaWQvMTE1/Mjg1MDExNi9waG90/by9sYXB0b3Atb24t/dGFibGUuanBnP3M9/NjEyeDYxMiZ3PTAm/az0yMCZjPW5wVjVs/ZHJzT0dsZ21XaHRf/U2FIMl9KVzRkU2J6/a0ZzMmJnT1ZZSksy/WFk9" class="card-img-top" alt="...">
+                <div class="card-body" style="height: 150px;">
+                    <p class="card-text title-productos">
+                        Some quick example text to build on the card title and make up the bulk of the card's content.
+                    </p>
+                    <p class="card-text price">
+                        $9.99
+                    </p>
                 </div>
             </div>
-        </div>
-        <br>
-        <div class="row">
-            <div class="col">
-                <div class="card" style="width: 18rem;">
-                    <img src="https://imgs.search.brave.com/FBUlJ2pW5zjmiWo69PXsfui0PR01IGoC8S78T5SKNxk/rs:fit:500:0:0/g:ce/aHR0cHM6Ly90NC5m/dGNkbi5uZXQvanBn/LzA1LzIyLzU0Lzgx/LzM2MF9GXzUyMjU0/ODEyOV9NT3p1WUQw/THV0bGVoa2dNME8w/M2ExT3JNc29yREtT/MS5qcGc" class="card-img-top" alt="...">
-                    <div class="card-body">
-                        <h5 class="card-title">Ram 32</h5>
-                        <p style="color: green;">$708</p>
-                        <p class="card-text">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Distinctio ducimus enim minus quos dicta accusamus illo odio mollitia ab, exercitationem dolorem, magnam, adipisci rerum quidem quam culpa velit rem similique.</p>
-                        <a href="#" class="btn btn-primary">Ver</a>
-                    </div>
+            <div class="card" style="width: 18rem; border: none; margin: 0px 5px;">
+                <img src="https://imgs.search.brave.com/eLsVxtv81rOFQe7sOKDmX9oW2JZh-6qbYTLnANedf4Y/rs:fit:500:0:0/g:ce/aHR0cHM6Ly9tZWRp/YS5pc3RvY2twaG90/by5jb20vaWQvMTE1/Mjg1MDExNi9waG90/by9sYXB0b3Atb24t/dGFibGUuanBnP3M9/NjEyeDYxMiZ3PTAm/az0yMCZjPW5wVjVs/ZHJzT0dsZ21XaHRf/U2FIMl9KVzRkU2J6/a0ZzMmJnT1ZZSksy/WFk9" class="card-img-top" alt="...">
+                <div class="card-body" style="height: 150px;">
+                    <p class="card-text title-productos">
+                        Some quick example text to build on the card title and make up the bulk of the card's content.
+                    </p>
+                    <p class="card-text price">
+                        $9.99
+                    </p>
                 </div>
             </div>
-            <div class="col">
-                <div class="card" style="width: 18rem;">
-                    <img src="https://imgs.search.brave.com/1N4_JZvBVEB6NxxN6cmdykDixcpiVxbIWFD89LxlVeM/rs:fit:500:0:0/g:ce/aHR0cHM6Ly9tLm1l/ZGlhLWFtYXpvbi5j/b20vaW1hZ2VzL0kv/NTF6aHVYeFl1Ukwu/anBn" class="card-img-top" alt="...">
-                    <div class="card-body">
-                        <h5 class="card-title">Disco duro 512</h5>
-                        <p style="color: green;">$1,500</p>
-                        <p class="card-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure eligendi sunt non facilis fugiat ipsa blanditiis doloribus, veniam modi tenetur cumque quia, sit delectus quas. Voluptatum unde vitae sed exercitationem!</p>
-                        <a href="#" class="btn btn-primary">Ver</a>
-                    </div>
+            <div class="card" style="width: 18rem; border: none; margin: 0px 5px;">
+                <img src="https://imgs.search.brave.com/eLsVxtv81rOFQe7sOKDmX9oW2JZh-6qbYTLnANedf4Y/rs:fit:500:0:0/g:ce/aHR0cHM6Ly9tZWRp/YS5pc3RvY2twaG90/by5jb20vaWQvMTE1/Mjg1MDExNi9waG90/by9sYXB0b3Atb24t/dGFibGUuanBnP3M9/NjEyeDYxMiZ3PTAm/az0yMCZjPW5wVjVs/ZHJzT0dsZ21XaHRf/U2FIMl9KVzRkU2J6/a0ZzMmJnT1ZZSksy/WFk9" class="card-img-top" alt="...">
+                <div class="card-body" style="height: 150px;">
+                    <p class="card-text title-productos">
+                        Some quick example text to build on the card title and make up the bulk of the card's content.
+                    </p>
+                    <p class="card-text price">
+                        $9.99
+                    </p>
                 </div>
             </div>
-            <div class="col">
-                <div class="card" style="width: 18rem;">
-                    <img src="https://imgs.search.brave.com/kBif1QdKBfyg11bPiYUPZjXJqa7APRuDxnT5A7HshT8/rs:fit:500:0:0/g:ce/aHR0cHM6Ly9tLm1l/ZGlhLWFtYXpvbi5j/b20vaW1hZ2VzL0kv/NzE4cW1yYlJTSkwu/anBn" class="card-img-top" alt="...">
-                    <div class="card-body">
-                        <h5 class="card-title">Disipador</h5>
-                        <p style="color: green;">$7,500</p>
-                        <p class="card-text">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Corrupti quos iure, omnis maxime natus ratione molestiae aut voluptate saepe nostrum? Aliquid rerum repellat officia corporis reprehenderit quo maxime at tempore?</p>
-                        <a href="#" class="btn btn-primary">Ver</a>
-                    </div>
+            <div class="card" style="width: 18rem; border: none; margin: 0px 5px;">
+                <img src="https://imgs.search.brave.com/eLsVxtv81rOFQe7sOKDmX9oW2JZh-6qbYTLnANedf4Y/rs:fit:500:0:0/g:ce/aHR0cHM6Ly9tZWRp/YS5pc3RvY2twaG90/by5jb20vaWQvMTE1/Mjg1MDExNi9waG90/by9sYXB0b3Atb24t/dGFibGUuanBnP3M9/NjEyeDYxMiZ3PTAm/az0yMCZjPW5wVjVs/ZHJzT0dsZ21XaHRf/U2FIMl9KVzRkU2J6/a0ZzMmJnT1ZZSksy/WFk9" class="card-img-top" alt="...">
+                <div class="card-body" style="height: 150px;">
+                    <p class="card-text title-productos">
+                        Some quick example text to build on the card title and make up the bulk of the card's content.
+                    </p>
+                    <p class="card-text price">
+                        $9.99
+                    </p>
                 </div>
             </div>
+            <div class="card" style="width: 18rem; border: none; margin: 0px 5px;">
+                <img src="https://imgs.search.brave.com/eLsVxtv81rOFQe7sOKDmX9oW2JZh-6qbYTLnANedf4Y/rs:fit:500:0:0/g:ce/aHR0cHM6Ly9tZWRp/YS5pc3RvY2twaG90/by5jb20vaWQvMTE1/Mjg1MDExNi9waG90/by9sYXB0b3Atb24t/dGFibGUuanBnP3M9/NjEyeDYxMiZ3PTAm/az0yMCZjPW5wVjVs/ZHJzT0dsZ21XaHRf/U2FIMl9KVzRkU2J6/a0ZzMmJnT1ZZSksy/WFk9" class="card-img-top" alt="...">
+                <div class="card-body" style="height: 150px;">
+                    <p class="card-text title-productos">
+                        Some quick example text to build on the card title and make up the bulk of the card's content.
+                    </p>
+                    <p class="card-text price">
+                        $9.99
+                    </p>
+                </div>
+            </div>
+            <div class="card" style="width: 18rem; border: none; margin: 0px 5px;">
+                <img src="https://imgs.search.brave.com/eLsVxtv81rOFQe7sOKDmX9oW2JZh-6qbYTLnANedf4Y/rs:fit:500:0:0/g:ce/aHR0cHM6Ly9tZWRp/YS5pc3RvY2twaG90/by5jb20vaWQvMTE1/Mjg1MDExNi9waG90/by9sYXB0b3Atb24t/dGFibGUuanBnP3M9/NjEyeDYxMiZ3PTAm/az0yMCZjPW5wVjVs/ZHJzT0dsZ21XaHRf/U2FIMl9KVzRkU2J6/a0ZzMmJnT1ZZSksy/WFk9" class="card-img-top" alt="...">
+                <div class="card-body" style="height: 150px;">
+                    <p class="card-text title-productos">
+                        Some quick example text to build on the card title and make up the bulk of the card's content.
+                    </p>
+                    <p class="card-text price">
+                        $9.99
+                    </p>
+                </div>
+            </div>
+
         </div>
     </div>
 
