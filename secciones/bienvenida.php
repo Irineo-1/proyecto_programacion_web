d2f7d4853b1d52e56dd50a4e39f742c03cb3b54e
Checo42 <sergio.perezherrera42@gmail.com>
2024-06-16 17:58:52 -0600

    Bienvenida prueba 1
    Se agrega el primer diseño sobra la pagina de bienvenida, el cual contiene los botones superiores.

---
+++ secciones/bienvenida.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Página de Bienvenida</title>
+    <link rel="stylesheet" href="secciones/css/bienvenida/bienvenida.css">
+</head>
+<body>
+    <div class="navbar">
+        <div class="navbar-left">
+            <button onclick="window.location.href=\'index.php\'">Inicio</button>
+        </div>
+        <div class="navbar-right">
+            <button onclick="window.location.href=\'productos.php\'">Productos</button>
+            <button onclick="window.location.href=\'login.php\'">Inicio de Sesión</button>
+        </div>
+    </div>
+    <div class="content">
+        <h1>Bienvenido a la Página</h1>
+        <p>Esta es la página de bienvenida.</p>
+    </div>
+    <script src="secciones/script.js"></script>
+</body>
+</html>';
+?>
