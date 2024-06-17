fba9614280c602a75864c51f0022e76f07a228ae
Checo42 <sergio.perezherrera42@gmail.com>
2024-06-16 18:19:34 -0600

    Update bienvenida.php

--- secciones/bienvenida.php
+++ secciones/bienvenida.php
@@ -1,13 +1,9 @@
 <section>
 
-<!DOCTYPE html>
-<html lang="es">
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Página de Bienvenida</title>
     <link rel="stylesheet" href="secciones/css/bienvenida/bienvenida.css">
 </head>
+
 <body>
     <div class="navbar">
         <div class="navbar-left">
