742fa14eb8663dd887cefe2ec03e16f1f781fd7f
Checo42 <sergio.perezherrera42@gmail.com>
2024-06-22 14:55:24 -0600

    Update bienvenida.php

--- secciones/bienvenida.php
+++ secciones/bienvenida.php
@@ -30,12 +30,16 @@
     </section>
 
     <script>
-        function scrollToTop() {
-            window.scrollTo({
-                top: 0,
-                behavior: 'smooth'
-            });
+    window.onscroll = function() {scrollFunction()};
+    
+    function scrollFunction() {
+        if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+            document.querySelector(".scroll-to-top").classList.add("show");
+        } else {
+            document.querySelector(".scroll-to-top").classList.remove("show");
         }
-    </script>
+    }
+</script>
+
 </body>
 </html>
