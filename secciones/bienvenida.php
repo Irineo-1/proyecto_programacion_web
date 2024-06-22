5cf1d377a037cd2c973c7fdf7ce43fd14981567a
Checo42 <sergio.perezherrera42@gmail.com>
2024-06-22 13:00:16 -0600

    Update bienvenida.php

--- secciones/bienvenida.php
+++ secciones/bienvenida.php
@@ -8,16 +8,19 @@
             <button onclick="window.location.href='login.php'">Inicio de Sesión</button>
         </div>
     </div>
-    <div class="header">
-        <img src="https://github.com/Irineo-1/proyecto_programacion_web/blob/bienvenida/secciones/css/bienvenida/Logo%20Pagina.png?raw=true" alt="TecnoParts Logo" class="logo">
-        <h1>¡Bienvenido a TecnoParts!</h1>
-        <p>Su socio de confianza en piezas tecnológicas.</p>
+    <div class="main-content">
+        <div class="header">
+            <img src="https://github.com/Irineo-1/proyecto_programacion_web/blob/bienvenida/secciones/css/bienvenida/Logo%20Pagina.png?raw=true" alt="TecnoParts Logo" class="logo">
+            <h1>¡Bienvenido a TecnoParts!</h1>
+            <p>Su socio de confianza en piezas tecnológicas.</p>
+        </div>
     </div>
     <a href="#" class="scroll-to-top">
         <img src="https://cdn-icons-png.freepik.com/256/8489/8489783.png" alt="Subir">
     </a>
 </section>
 <link rel="stylesheet" href="secciones/css/bienvenida/bienvenida.css">
+
 
 <script>
     window.onscroll = function() {scrollFunction()};
