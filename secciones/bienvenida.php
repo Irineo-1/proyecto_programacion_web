c7a876cf39ebc27e899d588bc728cf9147ae2517
Checo42 <sergio.perezherrera42@gmail.com>
2024-06-22 15:02:01 -0600

    Update bienvenida.php

--- secciones/bienvenida.php
+++ secciones/bienvenida.php
@@ -19,7 +19,7 @@
         </div>
         <div class="main-content">
             <div class="header">
-                <img src="https://github.com/Irineo-1/proyecto_programacion_web/blob/bienvenida/secciones/css/bienvenida/Logo%20Pagina.png?raw=true" alt="TecnoParts Logo" class="logo">
+                <img src="https://github.com/Irineo-1/proyecto_programacion_web/blob/bienvenida/utils/Logo%20Pagina.png?raw=true" alt="TecnoParts Logo" class="logo">
                 <h1>¡Bienvenido a TecnoParts!</h1>
                 <p>Su socio de confianza en piezas tecnológicas.</p>
             </div>
